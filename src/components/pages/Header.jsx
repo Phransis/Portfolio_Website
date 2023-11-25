d0dca64197b7e361d833b757a7e7af9758b4c069
Duo Francis <duofrancis11@gmil.com>
2023-11-25 08:32:17 +0000

    grids and columns

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import taikai1 from "../assets/taikai.png";
 
 function Header() {
-  return <div>
-    <img></img>
-    <h1>Handla's Website</h1>
-    
-  </div>;
+  return (
+    <>
+      <div className="headerBar">
+        <nav>
+          <img src={taikai1} alt="" className="image"></img>
+          <ul className="navbar-list">
+            <li>About</li>
+            <li>Projects</li>
+            <li>Skills</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+      {/* <div>Handla's Website</div> */}
+
+    </>
+  );
 }
 
 export default Header;
