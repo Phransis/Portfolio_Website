ac49ccbcf80581fc292668b8f426f7ce3874cfe9
Duo Francis <duofrancis11@gmil.com>
2024-08-06 11:12:52 +0000

    testing deployment chnges

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='footer'>
-        <div className='column1'>footer1</div>
+        <div className='column1'>footer1 test</div>
         <div className='column2'>footer2</div>
         <div className='column3'>footer3</div>
     </div>
